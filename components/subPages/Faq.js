0ccd01e294c19e0950ec0855d0143e0c59c9e7fc
boogerman919 <raytsai919@gmail.com>
2022-06-24 21:58:24 -0700

    finished FAQs page

--- components/subPages/Faq.js
+++ components/subPages/Faq.js
@@ -20,52 +20,50 @@ EStyleSheet.build({$rem: rem});
 const Faq = ({display}) => {
   return (
     <View style={styles.container} display={display}>
-      <View style={[styles.width100, styles.header]}>
-        <Text style={styles.title}>FAQs</Text>
-        <Text style={styles.subText} />
-      </View>
-      <ScrollView
+      {/* <ScrollView
         style={[styles.faqScroll]}
         contentContainerStyle={{flexGrow: 1}}
         removeClippedSubviews={true}>
-        <View style={styles.card}>
-          <Text style={styles.question}>When will I get charged?</Text>
-          <Text style={styles.answer}>
-            For now, the app is still in its alpha stage, and you won't be
-            charged :)
-          </Text>
-        </View>
-        <View style={styles.card}>
-          <Text style={styles.question}>
-            What is the mission of this startup?
-          </Text>
-          <Text style={styles.answer}>To make surfing simple and easy. :D</Text>
-        </View>
-        <View style={styles.card}>
-          <Text style={styles.question}>What is your startup's timeline</Text>
-          <Text style={styles.answer}>
-            There will be a lot of legal work to put our surfboard on more
-            beaches. We want to give easy surfboard access to everyone. {'\n\n'}
-            We look to put our first permanent rental stand in mid-2023.
-          </Text>
-        </View>
-      </ScrollView>
+      </ScrollView> */}
+      <Text style={styles.header}>FAQs</Text>
+      <View style={styles.bar} />
+      <Text style={styles.title}>How will I be charged?</Text>
+      <Text style={styles.text}>
+        A small deposit will be temporarily placed on hold when you start
+        renting. You will be charged according to the duration and the deposit
+        will be refunded.{' '}
+      </Text>
+      <View style={styles.bar} />
+      <Text style={styles.title}>What if I break the surfboards?</Text>
+      <Text style={styles.text}>
+        You should report the incident via Contact Us immediately. You will not
+        be charged for any damage.{' '}
+      </Text>
+      <View style={styles.bar} />
+      <Text style={styles.title}>Where do I return the surfboards?</Text>
+      <Text style={styles.text}>
+        You will need to return the surfboards back to the exact same slot in
+        the station, otherwise you cannot end the session.{' '}
+      </Text>
+      <View style={styles.bar} />
+      <Text style={styles.title}>What if my phone is not NFC compatible?</Text>
+      <Text style={styles.text}>
+        Unfortunately we only offer our service through NFC unlocking at this
+        stage.{' '}
+      </Text>
+      <View style={styles.bar} />
     </View>
   );
 };
 
 const styles = EStyleSheet.create({
-  width100: {
-    width: '100%',
-  },
   container: {
     backgroundColor: '#F8F8F8',
     width: '100%',
     height: '100%',
-    position: 'relative',
-    //top: '-61rem',
+    marginTop: '50rem',
     marginLeft: '-61rem',
-    flexGrow: 3,
+    alignItems: 'center',
   },
   faqScroll: {
     paddingLeft: '18rem',
@@ -74,40 +72,39 @@ const styles = EStyleSheet.create({
     width: '100%',
     height: window.height - 63 * rem,
   },
-  title: {
-    fontSize: '28rem',
+  header: {
+    fontSize: '22rem',
     color: 'black',
     width: '100%',
-    textAlign: 'right',
+    textAlign: 'center',
+    paddingBottom: '20rem',
     fontFamily: 'Montserrat-SemiBold',
   },
-  header: {
-    backgroundColor: '#07f2bd', // i made it darker just because, it might be a bad idea
-    paddingLeft: '18rem',
-    paddingRight: '18rem',
-    paddingTop: '23rem',
-    borderBottomWidth: 2,
-    borderColor: 'grey',
-    borderBottomLeftRadius: 5,
-    borderBottomRightRadius: 5,
+  bar: {
+    backgroundColor: 'black',
+    width: '85%',
+    borderWidth: '0.5rem',
+    marginTop: '20rem',
+    marginBottom: '20rem',
   },
-  question: {
+
+  title: {
     color: 'black',
-    fontSize: '22rem',
-    fontFamily: 'Montserrat-Medium',
+    fontSize: '16rem',
+    width: '90%',
+    textAlign: 'left',
+    fontFamily: 'Montserrat-SemiBold',
+    marginBottom: '5rem',
+    opacity: 0.7,
   },
-  answer: {
+  text: {
     color: 'black',
-    fontSize: '17rem',
-    fontFamily: 'Montserrat-Medium',
-    paddingTop: '4rem',
-    paddingLeft: '20rem',
-  },
-  card: {
-    paddingBottom: '18rem',
-    paddingTop: '10rem',
-    borderBottomWidth: 0.5,
-    borderColor: 'grey',
+    fontSize: '12rem',
+    width: '90%',
+    textAlign: 'left',
+    fontFamily: 'Montserrat-SemiBold',
+    opacity: 0.6,
+    lineHeight: 20,
   },
 });
 
